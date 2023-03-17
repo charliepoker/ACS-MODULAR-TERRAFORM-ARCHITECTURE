d2750a9eb2ca81adc764fc529641bb81f2491482
charliepoker <iheanachocharlie@gmail.com>
2023-03-17 17:58:34 -0500

    updated

--- modules/Autoscaling/asg-wordpress-tooling.tf
+++ modules/Autoscaling/asg-wordpress-tooling.tf
@@ -1,53 +1,14 @@
-# launch template for wordpress
-
-resource "aws_launch_template" "wordpress-launch-template" {
-  image_id               = var.ami
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.webserver-sg.id]
-
-  iam_instance_profile {
-    name = aws_iam_instance_profile.ip.id
-  }
-
-  key_name = var.keypair
-
-  placement {
-    availability_zone = "random_shuffle.az_list.result"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tag_specifications {
-    resource_type = "instance"
-
-    tags = merge(
-      var.tags,
-      {
-        Name = "wordpress-launch-template"
-      },
-    )
-
-  }
-
-  user_data = filebase64("${path.module}/wordpress.sh")
-}
-
 # ---- Autoscaling for wordpress application
 
 resource "aws_autoscaling_group" "wordpress-asg" {
   name                      = "wordpress-asg"
-  max_size                  = 2
-  min_size                  = 1
+  max_size                  = var.max_size
+  min_size                  = var.min_size
   health_check_grace_period = 300
   health_check_type         = "ELB"
-  desired_capacity          = 1
-  vpc_zone_identifier = [
+  desired_capacity          = var.desired_capacity
 
-    aws_subnet.private[0].id,
-    aws_subnet.private[1].id
-  ]
+  vpc_zone_identifier = var.private_subnets
 
   launch_template {
     id      = aws_launch_template.wordpress-launch-template.id
@@ -55,7 +16,7 @@ resource "aws_autoscaling_group" "wordpress-asg" {
   }
   tag {
     key                 = "Name"
-    value               = "wordpress-asg"
+    value               = "ACS-wordpress-asg"
     propagate_at_launch = true
   }
 }
@@ -63,59 +24,22 @@ resource "aws_autoscaling_group" "wordpress-asg" {
 # attaching autoscaling group of  wordpress application to internal loadbalancer
 resource "aws_autoscaling_attachment" "asg_attachment_wordpress" {
   autoscaling_group_name = aws_autoscaling_group.wordpress-asg.id
-  lb_target_group_arn    = aws_lb_target_group.wordpress-tgt.arn
+  lb_target_group_arn    = var.wordpress-alb-tgt
 }
 
-# launch template for toooling
-resource "aws_launch_template" "tooling-launch-template" {
-  image_id               = var.ami
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.webserver-sg.id]
 
-  iam_instance_profile {
-    name = aws_iam_instance_profile.ip.id
-  }
-
-  key_name = var.keypair
-
-  placement {
-    availability_zone = "random_shuffle.az_list.result"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tag_specifications {
-    resource_type = "instance"
-
-    tags = merge(
-      var.tags,
-      {
-        Name = "RCR-tooling"
-      },
-    )
-
-  }
-
-  user_data = filebase64("${path.module}/tooling.sh")
-}
 
 # ---- Autoscaling for tooling -----
 
 resource "aws_autoscaling_group" "tooling-asg" {
   name                      = "tooling-asg"
-  max_size                  = 2
-  min_size                  = 1
+  max_size                  = var.max_size
+  min_size                  = var.min_size
   health_check_grace_period = 300
   health_check_type         = "ELB"
-  desired_capacity          = 1
+  desired_capacity          = var.desired_capacity
 
-  vpc_zone_identifier = [
-
-    aws_subnet.private[0].id,
-    aws_subnet.private[1].id
-  ]
+  vpc_zone_identifier = var.private_subnets
 
   launch_template {
     id      = aws_launch_template.tooling-launch-template.id
@@ -124,12 +48,12 @@ resource "aws_autoscaling_group" "tooling-asg" {
 
   tag {
     key                 = "Name"
-    value               = "ASC-tooling"
+    value               = "ACS-tooling"
     propagate_at_launch = true
   }
 }
 # attaching autoscaling group of  tooling application to internal loadbalancer
 resource "aws_autoscaling_attachment" "asg_attachment_tooling" {
   autoscaling_group_name = aws_autoscaling_group.tooling-asg.id
-  lb_target_group_arn    = aws_lb_target_group.tooling-tgt.arn
+  lb_target_group_arn    = var.tooling-alb-tgt
 }
